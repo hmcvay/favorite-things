eee92b294f0ae261b3f7b004a9df4aac7f56ffaa
hmcvay <hayleymcvay8@gmail.com>
2022-01-18 11:33:05 -0800

    Add array loop to display results.
    Co-Authored-By: Mikah Mathews <mikah@outofband.org>

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+//blah blah get html Input and assign it
+$(document).ready(function() {
+  $("button#submit").click(function(event) {
+    event.preventDefault();
+    let results = [] // start with an empty array
+
+    let animal = $("#fav-animal").val();
+    let drink = $("#fav-drink").val();
+    let color = $("#fav-color").val();
+
+    results.push(animal); // either manually or with a loop add each input to the array
+    results.push(drink);
+    results.push(color);
+
+    console.log(results);
+
+    // for (i=0; i<results.length; i++) {
+    //   $(".array-results").append("<li>" + results[i] + "</li>");
+    // }
+
+    results.forEach(function(result) {
+      $(".array-results").append("<li>" + result + "</li>");
+    });
+  });
+});
